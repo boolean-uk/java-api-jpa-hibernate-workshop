eaa91d1de7b52f7a0a80a77df2e9309fecde11e2
Nikolaos Giagkinis <giagkinis.nick@gmail.com>
2023-06-05 14:08:19 +0300

    core excercise

---
+++ src/main/java/com/booleanuk/api/model/Teacher.java
@@ -0,0 +1,76 @@
+package com.booleanuk.api.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "teachers")
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "subject")
+    private String subject;
+
+    @Column(name = "email")
+    private String email;
+
+    public Teacher() {
+    }
+
+    public Teacher(int id, String name, String location, String subject, String email) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.subject = subject;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
