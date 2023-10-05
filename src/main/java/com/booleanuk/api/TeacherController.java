b64d12e8897fbac6a3d996612e3bd00e56ce0368
Irene <irene.maniou@hotmail.com>
2023-10-05 14:38:21 +0300

    Irene Maniou

---
+++ src/main/java/com/booleanuk/api/TeacherController.java
@@ -0,0 +1,50 @@
+package com.booleanuk.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("teachers")
+public class TeacherController {
+
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    @GetMapping
+    public List<Teacher> getAll() {
+        return this.teacherRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Teacher> createTeacher(@RequestBody Teacher teacher) {
+        return new ResponseEntity<>(this.teacherRepository.save(teacher), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Teacher> updateTeacher(@PathVariable int id, @RequestBody Teacher teacher) {
+        Teacher teacherToUpdate = this.teacherRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found for updating"));
+
+        teacherToUpdate.setName(teacher.getName());
+        teacherToUpdate.setLocation(teacher.getLocation());
+        teacherToUpdate.setSubject(teacher.getSubject());
+        teacherToUpdate.setEmail(teacher.getEmail());
+
+        return new ResponseEntity<>(this.teacherRepository.save(teacherToUpdate), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Teacher> deleteTeacher(@PathVariable int id, @RequestBody Teacher teacher) {
+        Teacher teacherToDelete = this.teacherRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found for deleting"));
+
+        this.teacherRepository.delete(teacherToDelete);
+        return ResponseEntity.ok(teacherToDelete);
+    }
+}
